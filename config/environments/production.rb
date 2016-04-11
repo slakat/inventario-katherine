4fdebb43bd9a3ee8620bb2b095038a1de77e950c
Katherine <slakat@gmail.com>
2016-04-10 22:09:32 -0300

    cambios en variables

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -76,10 +76,7 @@ Rails.application.configure do
     :enable_starttls_auto => true
   }
 
-  config.action_mailer.default_url_options = {
-    host: ENV.fetch('HOST'),
-    protocol: ENV.fetch('PROTOCOL')
-  }
+  config.action_mailer.delivery_method = :smtp
 
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
   # the I18n.default_locale when a translation cannot be found).
